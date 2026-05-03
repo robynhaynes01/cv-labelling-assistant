bc61896accd73ebd6c8ba898fbde86f2da11f212
Robyn <ryan.haynes374@gmail.com>
2026-05-03 04:19:10 +0100

    layout plan for how creation should work

---
+++ src/dataset_manager/creator.py
@@ -0,0 +1,12 @@
+def create_new_dataset(dataset_name: str):
+    # Initially, dataset creation is relying on a folder structure
+    # that is flat. In the future, this can be expended to support more
+    # dynamic structures.
+    # For now, we will create a folder structure like:
+    # dataset_name/
+    #   img1.jpg
+    #   img1.txt
+    #   img2.jpg
+    #   img2.txt
+    #   ...
+    pass
