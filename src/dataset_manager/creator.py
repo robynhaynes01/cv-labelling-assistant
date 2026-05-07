e6180738a0bd2717a5d6154d41a1f1b9dccc444d
Robyn Haynes <ryan.haynes374@gmail.com>
2026-05-07 19:15:37 +0100

    isolation for images and labels

--- src/dataset_manager/creator.py
+++ src/dataset_manager/creator.py
@@ -1,4 +1,15 @@
+import os
 from dataset_manager.validator import validate_import_image_and_label_formats
+from dataset_manager.common import VALID_IMAGE_FORMATS, VALID_LABEL_FORMATS
+
+def isolate_labels(data_path) -> list[str]:
+    label_files = [file for file in os.listdir(data_path) if os.path.splitext(file)[1] in VALID_LABEL_FORMATS]
+    return label_files
+
+def isolate_images(data_path) -> list[str]:
+    image_files = [file for file in os.listdir(data_path) if os.path.splitext(file)[1] in VALID_IMAGE_FORMATS]
+    return image_files
+
 
 def create_new_dataset(dataset_name, data_path):
     # Initially, dataset creation is relying on a folder structure
