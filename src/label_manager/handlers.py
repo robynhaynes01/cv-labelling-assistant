85d81ae4023f4a33ae3727511a0e9f91722d71dd
Robyn Haynes <ryan.haynes374@gmail.com>
2026-05-07 20:51:20 +0100

    load and save functionality for json and txt formats

--- src/label_manager/handlers.py
+++ src/label_manager/handlers.py
@@ -1,3 +1,4 @@
+import json
 class LabelHandlerInterface:
     def load_labels(self, file_path: str) -> list:
         raise NotImplementedError
@@ -8,18 +9,32 @@ class LabelHandlerInterface:
     def convert_labels(self, labels: list, target_format: str) -> list:
         raise NotImplementedError
     
-    def _determine_format(self, file_path: str) -> str:
+    def _determine_label_format(self, file_path: str) -> str:
         raise NotImplementedError
     
-
 class LabelTXTHandler(LabelHandlerInterface):
-    pass
+    def load_labels(self, file_path: str) -> list:
+        with open(file_path, 'r') as f:
+            labels = f.readlines()
+        return [label.strip() for label in labels]
+    
+    def save_labels(self, labels: list, file_path: str) -> None:
+        with open(file_path, 'w') as f:
+            for label in labels:
+                f.write(f"{label}\n")
 
 class LabelXMLHandler(LabelHandlerInterface):
     pass
 
 class LabelJSONHandler(LabelHandlerInterface):
-    pass
+    def load_labels(self, file_path: str) -> list:
+        with open(file_path, 'r') as f:
+            labels = json.load(f)
+        return labels
+    
+    def save_labels(self, labels: list, file_path: str) -> None:
+        with open(file_path, 'w') as f:
+            json.dump(labels, f, indent=4)
 
 
 if __name__ == '__main__':
