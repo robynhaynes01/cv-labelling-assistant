4c36a1419f69be585bffd2a120f92f01e2f00494
Robyn Haynes <ryan.haynes374@gmail.com>
2026-05-29 17:34:48 +0100

    additions to common

--- src/model_manager/common.py
+++ src/model_manager/common.py
@@ -3,7 +3,5 @@ from ultralytics import settings
 RUNS_DIR = settings.get('runs_dir')
 DATASETS_DIR = 'datasets'
 
-SUPPORTED_TASKS = ["detect"]
-
-print(f"Runs: {RUNS_DIR}")
-print(f"Datasets: {DATASETS_DIR}")
+DETECT = "detect"
+SUPPORTED_TASKS = [DETECT]
